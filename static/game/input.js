158346ddb8929f579f65cba82f1b10c2b8333ec6
ndyumin <nikita.dyumin@gmail.com>
2015-12-12 00:22:23 +0500

    enable user input by clicks

---
+++ static/game/input.js
@@ -0,0 +1,26 @@
+/**
+ * Created by ndyumin on 11.12.2015.
+ */
+define(['jquery', './dict'], function($, dict) {
+    'use strict';
+    return function(sel) {
+        const domElement = document.querySelector(sel);
+        const clickS = $(domElement).asEventStream('click');
+
+        function toPixelCoords (evt) {
+            var rect = domElement.getBoundingClientRect();
+            return [
+                evt.clientX - rect.left,
+                evt.clientY - rect.top
+            ];
+        }
+
+        function toCellCoords (coords) {
+            return coords.map(c => Math.floor(c / dict.GRID.CELL_SIZE * dict.VIEWPORT.RATIO));
+        }
+
+        return clickS
+            .map(toPixelCoords)
+            .map(toCellCoords);
+    }
+});
