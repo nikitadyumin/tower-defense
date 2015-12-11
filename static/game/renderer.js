2b5ed6d8d6bc0c677745b0fb07c4dbaefcab4619
ndyumin <nikita.dyumin@gmail.com>
2015-12-11 11:26:40 +0500

    visual enhancements

--- static/game/renderer.js
+++ static/game/renderer.js
@@ -11,17 +11,54 @@ define(['jquery', './dict'], function ($, dict) {
         const height = $canvas.height();
         const cellSize = dict.GRID.CELL_SIZE;
 
+        //todo move to color module
+        function toRgbString(rgb) {
+            return `rgb(${rgb})`;
+        }
+        function shiftColor(rgb, v) {
+            return rgb.map(x=>x+v);
+        }
+
+        function drawCell (x, y, color) {
+            ctx.fillStyle = toRgbString(color);
+            ctx.fillRect(x, y, cellSize, cellSize);
+
+            ctx.fillStyle = toRgbString(shiftColor(color, 20));
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(x, y + cellSize);
+            ctx.lineTo(x + cellSize, y);
+            ctx.closePath();
+            ctx.fill();
+
+            ctx.fillStyle = toRgbString(shiftColor(color, 10));
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(x, y + cellSize);
+            ctx.lineTo(x + cellSize / 2, y + cellSize / 2);
+            ctx.closePath();
+            ctx.fill();
+
+            ctx.fillStyle =  toRgbString(shiftColor(color, -10));
+            ctx.beginPath();
+            ctx.moveTo(x + cellSize, y + cellSize);
+            ctx.lineTo(x, y + cellSize);
+            ctx.lineTo(x + cellSize / 2, y + cellSize / 2);
+            ctx.closePath();
+            ctx.fill();
+        }
+        
         function getColor(c) {
             const colors = {};
-            colors[dict.TERRAIN.TRAVERSABLE] = '#9aafb7';
-            colors[dict.TERRAIN.NOT_TRAVERSABLE] = '#2b3b75';
-            colors[dict.TERRAIN.SPAWN] = '#7181b2';
-            colors[dict.TERRAIN.GOAL] = '#496815';
-            return colors[c] || 'black';
+            colors[dict.TERRAIN.TRAVERSABLE] = [154, 175, 183]; //'#9aafb7';
+            colors[dict.TERRAIN.NOT_TRAVERSABLE] = [43, 59, 117]; //'#2b3b75';
+            colors[dict.TERRAIN.SPAWN] = [113, 129, 178]; //'#7181b2';
+            colors[dict.TERRAIN.GOAL] = [73, 104, 21]; //'#496815';
+            return colors[c] || [0, 0, 0];
         }
 
         function clean(ctx) {
-            ctx.fillStyle = getColor();
+            ctx.fillStyle = toRgbString(getColor());
             ctx.fillRect(0, 0, width, height);
         }
 
@@ -31,8 +68,12 @@ define(['jquery', './dict'], function ($, dict) {
             clean(ctx);
             map.grid.forEach((line, y) =>
                 line.forEach((cell, x) => {
-                    ctx.fillStyle = getColor(cell);
-                    ctx.fillRect(cellSize * x, cellSize * y, cellSize, cellSize);
+                    if (cell === dict.TERRAIN.NOT_TRAVERSABLE) {
+                        drawCell(cellSize*x, cellSize*y, getColor(cell));
+                    } else {
+                        ctx.fillStyle = toRgbString(getColor(cell));
+                        ctx.fillRect(cellSize * x, cellSize * y, cellSize, cellSize);
+                    }
                 }));
 
             state.enemies.forEach(enemy => {
