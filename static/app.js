bcb43c822f883e28c8751eb82003ded2fbf4cffe
ndyumin <nikita.dyumin@gmail.com>
2015-12-12 13:34:11 +0500

    added fullscreen toggling button

--- static/app.js
+++ static/app.js
@@ -17,10 +17,34 @@ requirejs.config({
     }
 });
 
-require(['bacon', './router'], function (Bacon, router) {
-    function getLocationHash () {
+require(['jquery', 'bacon', './game/dict', './router'], function ($, Bacon, dict, router) {
+    function enableFullScreen(el) {
+        const request = el.requestFullScreen || el.mozRequestFullScreen || el.webkitRequestFullScreen;
+        const cancel = document.cancelFullScreen || document.mozCancelFullScreen || document.webkitCancelFullScreen;
+
+        return function () {
+            if (document.isFullScreen || document.mozIsFullScreen || document.webkitIsFullScreen) {
+                if (cancel) {
+                    cancel.call(document)
+                }
+            } else {
+                if (request) {
+                    request.call(el);
+                }
+            }
+        };
+    }
+
+    function getLocationHash() {
         return location.hash
     }
+
+    const $sel = $(dict.VIEWPORT.SELECTOR);
+    const $fullScreenToggle = $('button#fullscreen');
+
+    $sel.width($sel.width() / dict.VIEWPORT.RATIO);
+    $fullScreenToggle.asEventStream('click')
+        .onValue(enableFullScreen(document.documentElement));
 
     Bacon.fromEventTarget(window, 'hashchange')
         .toProperty('')
